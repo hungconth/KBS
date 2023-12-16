5acd4339206d025485875f91b5acbca2a603f4f9
hungconth <hung8a1thth@gmail.com>
2023-12-16 16:47:48 +0700

    app

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,21 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Block from "./Block";
 import { title } from "process";
-
+import axios from "axios";
+import { text } from "stream/consumers";
 
 
 
 export default function SearchInput() {
     const [textMe, setTextMe] = useState('')
     const [myArray, setMyArray] = useState<any>([])
-
-    const handleAdd = (title: string) => {
-        if(title === '')alert('Nhập từ.')
-        else{
-            setMyArray([...myArray, title])
-            setTextMe('')
+    
+    /// api check tu xem co trong tu dien khong
+    const handleClickPos = async () =>{
+        if(textMe==="")alert('Nhập từ.')
+        else
+        try{
+            const response = await fetch("http://localhost:8000/check-pos/", {
+                method: 'POST',
+                headers: {
+                'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ word: textMe }),
+            });
+        
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            
+            const data = await response.json();
+            if(data.in_dict === true){
+                setMyArray([...myArray, textMe])
+                setTextMe('')
+            }
+            else{
+                alert("Từ không có trong dữ liệu. Vui lòng nhập lại! (đối với danh từ riêng phải viết hoa chữ cái đầu)")
+            }
+            
+        } catch (error) {
+          console.error('Fetch error:', error);
         }
     }
+
     const handleDelete = (index: number) => {
         setMyArray(
             myArray.filter((item: any, idx: number) => {
@@ -23,17 +48,50 @@ export default function SearchInput() {
             })
         )
     }
+
+
     const [param, setParam] = useState('')
-    const handleSubmit = () => {
-        var strng = ''
-        for (let index = 0; index < myArray.length; index++) {
-            const element = myArray[index];
-            strng += element
+    const handleSubmit = async () => {
+        if(myArray.length <= 1)alert('Không đủ số từ yêu cầu, người dùng cần nhập thêm từ để thực hiện sắp xếp.')
+        else{try{
+            const response = await fetch("http://localhost:8000/generate-sentence/",
+            {
+                method: 'POST',
+                headers: {
+                'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ words: myArray }),
+            });
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            const data= await response.json();
+            console.log(data)
+            // setResultSubmit(data)
+            var strng = ''
+            for (let index = 0; index < (data.sentence.length >=2 ? 2 : data.sentence.length); index++) {
+                const element = data.sentence[index];
+                
+                    const yourArray = element[0]
+                    if(index <= 2) strng += index + '.'
+                    for (let z = 0; z < yourArray.length; z++) {
+                        strng += yourArray[z] + ' '
+                    }               
+            }
+            setParam(strng)
         }
-        setParam(strng)
+        catch(error){
+            console.error("Fetch error:",error)
+        }}
         
     }
-    console.log(myArray)
+
+    const handleDeleteAll = () =>{
+        setMyArray([])
+        setParam('')
+    
+    }
+    console.log(param)
     return (
 
         <>
@@ -48,14 +106,15 @@ export default function SearchInput() {
                     </div>
                     <input value={textMe} onChange={(e: any) => setTextMe(e.target.value)}
                         className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500  dark:placeholder-gray-400 text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Điền từ vào đây ....." required />
-                    <div onClick={() => handleAdd(textMe)}
+                    {/* <div onClick={() => handleAdd(textMe)} */}
+                    <div onClick={() => handleClickPos()}
                         className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Click!!</div>
                 </div>
             </div>
             <div className="flex flex-col mx-2">
                 {
                     myArray.length > 0 ?
-                        <>
+                        <> 
                             <ul className="flex flex-row flex-wrap">
                                 {
                                     myArray.map((item: any, index: number) => (
@@ -66,6 +125,11 @@ export default function SearchInput() {
                                 }
 
                             </ul>
+                            <button type="button" onClick={handleDeleteAll}
+                                className="inline-flex items-center text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-xs px-5 py-2.5 text-center dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-900 w-20">
+                                {/* <svg aria-hidden="true" className="w-5 h-5 mr-1.5 -ml-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path></svg> */}
+                                Delete All
+                            </button>
                             <div className='flex justify-between'>
                                 <div> </div>
                                 <button type="button" onClick={handleSubmit}
@@ -83,6 +147,7 @@ export default function SearchInput() {
                 <div className="m-5 border border-slate-600 border-4 rounded-md">
                     <div className="flex flex-row flex-wrap justify-center text-lg text-gray-900 md:text-xl dark:text-black font-bold">
                         {param}
+
                     </div>
 
                 </div>:<></>
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export default function Page() {
         <>
             
             <div className="xl:mx-96 md:mx-20 border-x border-slate-300 h-auto">
-                <h1 className="text-3xl font-bold text-cyan-300 ml-2 mt-2">Hệ thống dựa trên tri thức.</h1>
+                <h1 className="text-3xl font-bold text-cyan-300 ml-2 mt-2">Hệ thống sắp xếp các từ rời rạc thành câu có nghĩa.</h1>
                 <SearchInput />
             </div>
             
